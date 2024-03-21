f306b7065de1cc57fb1a96f74bb3421127b83701
Yadvendra Sharma <yadvendras20@gmail.com>
2024-03-21 15:20:59 +0530

    fixing css

--- src/Components/FAQ.jsx
+++ src/Components/FAQ.jsx
@@ -3,15 +3,15 @@ import FaqCard from './FaqCard'
 
 export default function FAQ() {
     const FaqList=[
-        {q:"What is Algomojo?",a:"Algomojo is an API-based Trading Platform to provide DIY (Do-It-Yourself) traders to build their trading strategies and automate in their own trading environment."},
-        {q:"What are the services provided by Algomojo?",a:"Algomojo provided end to end requirements of an Algo trader right from Automated Trading Education, API connectivity to multiple brokers, Execution module and sample codes to multiple trading platforms, Datafeed to the various platforms, trading bridge, Datafeed API, Virtual servers, free strategy libraries, Code development, and community support."},
-        {q:"Is the Algomojo Platform is Free to Access?",a:"Yes, Algomojo is free to access if the trading account is opened via Algomojo. If the trader already holds an existing account with the algomojo supported broker and wish to continue with the same account then algomojo platform pricing is applicable."},
-        {q:"Will there be any free trial available?",a:"Yes, 7 days of free trial for existing broker clients. Those who open a trading account via algomojo get lifetime access to Algomojo platform."},
-        {q:"What is the trading platform support by the Algomojo Platform?",a:"Algomojo Supports Amibroker, Tradingview, Excel, Metatrader, Python, C# based Applications."},
-        {q:"Who are the supported brokers?",a:"Currently, Algomojo Supports Angel Broking, Alice Blue, Fyers, Firstock, Samco, Tradejini, Upstox, Zebu, Zerodha"},
-        {q:"Can you recommend which broker to open a trading account?",a:"We are broker-neutral and we like to provide a wide array of choices and the users of algomojo can choose their own favorite brokers."},
-        {q:"Does Algomojo provides any guarantee on returns?",a:"Absolutely Not. The trader has to take complete responsibility for their trading strategies. Algomojo will neither take any responsibility for the returns of traders."},
-        {q:"How Secure is Algomojo?",a:"Algomojo provides enterprise-level security for the users and provides a resilient, secure, extensible, and scalable environment for the users"}
+        {q:"What is XYZ...?",a:"XYZ... is an API-based Trading Platform to provide DIY (Do-It-Yourself) traders to build their trading strategies and automate in their own trading environment."},
+        {q:"What are the services provided by XYZ...?",a:"XYZ... provided end to end requirements of an Algo trader right from Automated Trading Education, API connectivity to multiple brokers, Execution module and sample codes to multiple trading platforms, Datafeed to the various platforms, trading bridge, Datafeed API, Virtual servers, free strategy libraries, Code development, and community support."},
+        {q:"Is the XYZ... Platform is Free to Access?",a:"Yes, XYZ... is free to access if the trading account is opened via XYZ.... If the trader already holds an existing account with the XYZ... supported broker and wish to continue with the same account then XYZ... platform pricing is applicable."},
+        {q:"Will there be any free trial available?",a:"Yes, 7 days of free trial for existing broker clients. Those who open a trading account via XYZ... get lifetime access to XYZ... platform."},
+        {q:"What is the trading platform support by the XYZ... Platform?",a:"XYZ... Supports Amibroker, Tradingview, Excel, Metatrader, Python, C# based Applications."},
+        {q:"Who are the supported brokers?",a:"Currently, XYZ... Supports Angel Broking, Alice Blue, Fyers, Firstock, Samco, Tradejini, Upstox, Zebu, Zerodha"},
+        {q:"Can you recommend which broker to open a trading account?",a:"We are broker-neutral and we like to provide a wide array of choices and the users of XYZ... can choose their own favorite brokers."},
+        {q:"Does XYZ... provides any guarantee on returns?",a:"Absolutely Not. The trader has to take complete responsibility for their trading strategies. XYZ... will neither take any responsibility for the returns of traders."},
+        {q:"How Secure is XYZ...?",a:"XYZ... provides enterprise-level security for the users and provides a resilient, secure, extensible, and scalable environment for the users"}
 
     ]
   return (
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
     <div className='flex justify-between h-[60vh] w-[100%] bg-purple-500 px-16 '>
         <div className='flex flex-col justify-between align-bottom py-28 w-1/2 text-white'>
             <div className=' font-bold text-5xl'>XYZ...</div>
-            <div className='  text-4xl'>India's First Web Based FREE* API Algo Trading Execution Platform</div>
-            <div className=''>Place your strategy algo orders from Tradingview, ChartInk, Amibroker, Ninjatrader, MT4, MT5, Excel, Google Sheet, Google Chrome Extension, Python, and C#</div>
+            <div className='  text-4xl'>Web Based FREE* API Algo Trading Execution Platform</div>
+            <div className='font-semibold'>Place your strategy algo orders on Upstox using simple API calls using your coustom frontend like react, vew, etc, or any scripting language like JavaScript, python, c, etc.</div>
             {/* <div>
             <button className='mr-2 px-12 text-2xl py-1 bg-black text-purple-600 font-bold text-white rounded'>Login</button>
             <button className='mr-2 px-12 text-2xl py-1 bg-black text-purple-600 font-bold text-white rounded'>Login</button>
