77d84812439150f982dbbf8efb082c6de9fbb750
Yadvendra Sharma <yadvendras20@gmail.com>
2024-03-21 18:10:55 +0530

    added postman docs link

--- src/Components/HowItWorks.jsx
+++ src/Components/HowItWorks.jsx
@@ -4,7 +4,8 @@ export default function HowItWorks() {
   return (
     <div className='bg-purple-600 w-[100%] flex justify-center p-5'>
         <span className='text-2xl font-bold'>Go see the Docs to see how to use it.</span>
-        <button onClick={()=>{window.location.href = "https://documenter.getpostman.com/view/27161625/2sA35A6jeK";}} className='ml-10  bg-white text-purple-600 text-xl font-bold hover:shadow-lg shadow-black px-4  py-1 rounded-lg'>Docs</button>
+        <button onClick={() => { window.open("https://documenter.getpostman.com/view/27161625/2sA35A6jeK", "_blank"); }} className='ml-10 bg-white text-purple-600 text-xl font-bold hover:shadow-lg shadow-black px-4 py-1 rounded-lg'>Docs</button>
+
     </div>
   )
 }
